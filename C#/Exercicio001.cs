ac698ba37a9eb02c53286ba54e2821397d2313e4
Dante <90792084+DantteOwO@users.noreply.github.com>
2022-09-26 21:55:47 -0300

    melhora da emplementaçao

--- C#/Exercicio001.cs
+++
@@ -1,8 +0,0 @@
-using System;
-class Programa
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Olá, mundo. Este é meu primeiro programa!");
-    }
-}
---
+++ CSharp/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Programa
+{
+
+       static void Main(string[] args)
+    {
+        Console.WriteLine("Ola, Mundo");
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    } 
+       
+}
