5ef2685b93ce7e103ce4e9632649c8f22188aebf
Dante <90792084+DantteOwO@users.noreply.github.com>
2022-10-03 21:31:22 -0300

    Novos Exercicios de C#

---
+++ CSharp/Exercicio003/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variaveis");
+
+        int idade;
+
+       idade = 20;
+        
+        Console.WriteLine("A idade do carlos é..." + idade);
+
+        idade = 20 - 5;
+
+        Console.WriteLine("A idade de Ana é.." + idade);
+
+        idade = 5 * 2 - 6;
+
+        Console.WriteLine("Minha idade é..." + idade);
+
+        idade = (5 - 2) * 3;
+
+        Console.WriteLine("Silvana tem " + idade + " anos");
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }  
+}
---
+++ CSharp/Exercicio004/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 -  Variavel Ponto Flutuante");
+        double salario;
+        salario = 3000.13;
+
+        double idade;
+        idade = 7.0 / 5.0;
+
+        Console.WriteLine(idade);
+
+        Console.WriteLine(salario);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+
+    } 
+ }
