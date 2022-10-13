450540b37daacb130b38920e66729fda74bf3001
Dante <90792084+DantteOwO@users.noreply.github.com>
2022-10-12 21:18:42 -0300

    Exercicio005

---
+++ CSharp/Exercicio005/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Programa
+{
+
+    static void Main(string[] args)
+    {
+        double salario;
+        salario = 3000.15;
+
+        int salarioInteiro;
+        salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro); 
+
+        long x;
+        x = 1000000000000000;
+        Console.WriteLine(x);
+
+        short y;
+        y = 15000;
+        Console.Write(y);
+
+        Console.WriteLine(" Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+
+}
