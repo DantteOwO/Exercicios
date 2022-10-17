041fa2e08f5f17616d39e0bf0c3104321ee6b29e
Dante <90792084+DantteOwO@users.noreply.github.com>
2022-10-17 19:41:37 -0300

    Exercicio009

---
+++ CSharp/Exercicio009/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Programa
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executado o projeto 9 - Condifionais");
+
+        int idadeJoao = 16;
+
+        if (idadeJoao >= 18)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else 
+        {
+            Console.WriteLine("Nao pode entrar!");
+        }
+
+        Console.WriteLine("Tecle enter para fechar.... ");
+        Console.ReadLine();
+    }
+}
