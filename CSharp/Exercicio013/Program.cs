082ce46f08e5805eb7d442bcda70dabb58072fac
Dante <90792084+DantteOwO@users.noreply.github.com>
2022-10-19 22:03:06 -0300

    Exercicios

---
+++ CSharp/Exercicio013/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 13 - Calcula Poupança 2");
+
+        double investimento = 1000;
+
+        for (int mes = 1; mes <= 12; mes = mes + 1)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " você tem R$ " + investimento);
+
+        }
+
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
---
+++ CSharp/Exercicio015/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 15 - Encadeando For");
+
+        for (int contadorLinhas = 0; contadorLinhas <10; contadorLinhas++)
+        {
+            for(int contadorColunas = 0; contadorColunas <10; contadorColunas++)
+            {
+                Console.Write("*");
+                if (contadorColunas >= contadorLinhas)
+                    break;
+            }
+            Console.WriteLine();
+        }
+        
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
