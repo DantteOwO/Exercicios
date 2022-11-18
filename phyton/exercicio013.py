db049cff334ae62a33be62681406b1ab867aaa9e
Dante <90792084+DantteOwO@users.noreply.github.com>
2022-11-17 22:30:53 -0300

    exercicios

---
+++ phyton/exercicio013.py
@@ -0,0 +1,3 @@
+salario = float(input('Qual é o salario do Funcionario? R$'))
+novo = salario + (salario * 15 / 100)
+print('Um funcionario que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2F}'.format(salario, novo))
