969db45070dcb221fc6715363bdff0a6bf30bbd7
Dante <90792084+DantteOwO@users.noreply.github.com>
2022-11-17 23:15:20 -0300

    exercicios

--- phyton/exercicio015.py
+++ phyton/exercicio015.py
@@ -1,4 +1,20 @@
-dias = int(input('Quantos dias alugados? '))
-km = float(input('Quantos Km rodados? '))
-pago = (dias * 60) + (km * 0.15)
-print('O total a pagar é de R${:.2f}' .format(pago))
+import math
+import turtle
+
+def xt(t):
+    return 16*math.sin(t)**3
+
+def yt(t):
+    return 13*math.cos(t)-5*\
+            math.cos(2*t)-2*\
+            math.cos(3*t)-\
+            math.cos(4*t)
+
+t = turtle.Turtle()
+t.speed(500)
+turtle.bgcolor('black')
+
+for i in range(2550):
+    t.goto((xt(i)*20, yt(i)*20))
+    t.pencolor('red')
+    t.goto(0, 0)
