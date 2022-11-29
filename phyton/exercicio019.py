92bf2d839f206d8fbcedbd88756afc29fdc70bf8
Dante <90792084+DantteOwO@users.noreply.github.com>
2022-11-28 22:02:21 -0300

    exercicio

--- phyton/exercicio019.py
+++ phyton/exercicio019.py
@@ -2,3 +2,5 @@ import math
 
 n = float(input('Digite um numero: '))
 print('O valor digitado foi {} e a sua porção inteira é {} '.format(n, math.trunc(n)))
+
+# Digite um numero com virgola para saber somente seu inteiro
