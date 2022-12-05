755e461cfd63fa84ff86aa556f166bad02e26892
Dante <90792084+DantteOwO@users.noreply.github.com>
2022-12-04 21:56:27 -0300

    exercicio

---
+++ phyton/exercicio023.py
@@ -0,0 +1,10 @@
+from random import shuffle
+
+n1 = input('Primeiro aluno: ')
+n2 = input('Segundo aluno: ')
+n3 = input('Terceiro aluno: ')
+n4 = input('Quarto aluno: ')
+lista = (n1, n2, n3, n4)
+shuffle(lista)
+print('A ordem de apresentaçao sera..')
+print(lista)
